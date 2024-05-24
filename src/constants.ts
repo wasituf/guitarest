7acdbabe283e5cb965b845d9733aee71789b247a
wasituf <wasit.allin1@gmainl.com>
2024-05-24 08:25:13 +0600

    Added smoothing to the frequencies

--- src/constants.ts
+++ src/constants.ts
@@ -7,10 +7,10 @@ interface Note {
 }
 
 export const guitarStringRanges: Note[] = [
-  { name: "E2", min: 80.0, max: 85.0, perfectMin: 82.0, perfectMax: 82.8 },
-  { name: "A2", min: 105.0, max: 120.0, perfectMin: 109.7, perfectMax: 110.3 },
-  { name: "D3", min: 137.0, max: 155.0, perfectMin: 146.3, perfectMax: 147.0 },
-  { name: "G3", min: 194.0, max: 198.0, perfectMin: 195.7, perfectMax: 196.3 },
-  { name: "B3", min: 244.0, max: 250.0, perfectMin: 245.7, perfectMax: 246.3 },
-  { name: "E4", min: 326.0, max: 332.0, perfectMin: 328.7, perfectMax: 329.3 },
+  { name: "E2", min: 77.0, max: 88.0, perfectMin: 81.8, perfectMax: 83.0 },
+  { name: "A2", min: 105.0, max: 120.0, perfectMin: 109.3, perfectMax: 110.5 },
+  { name: "D3", min: 137.0, max: 155.0, perfectMin: 146.0, perfectMax: 147.2 },
+  { name: "G3", min: 190.0, max: 205.0, perfectMin: 195.3, perfectMax: 196.5 },
+  { name: "B3", min: 240.0, max: 252.0, perfectMin: 245.3, perfectMax: 246.5 },
+  { name: "E4", min: 323.0, max: 335.0, perfectMin: 328.3, perfectMax: 329.5 },
 ];
