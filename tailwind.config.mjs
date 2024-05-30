ba35781869baf96b7d55a3f6c6f4394ec47dd6ee
wasituf <wasit.allin1@gmainl.com>
2024-05-30 19:29:36 +0600

    New functionality
    Added the metronome with different timings. Changed some visual stuff
    and fixed some minor issues with the tuner.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Quicksand Variable", "sans-serif"],
+    },
+    extend: {
+      transitionProperty: {
+        size: "height, width",
+      },
+    },
+  },
+  plugins: [],
+};
